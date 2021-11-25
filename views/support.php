3c6e85751fbe6b1bb826a2a9c4daee0017799d25
Armanul46 <arman.sovware@gmail.com>
2021-11-26 00:25:50 +0600

    fixed linking

--- views/support.php
+++ views/support.php
@@ -1,29 +1,12 @@
 <div class="legal-page-support wplp-support-card wplp-container-sm wplp-ml-10 wplp-mt-30">
-    <h1 class="wplp-page-title">Usage & Support</h1>
     <div class="legal-page-support-container">
         <div class="legal-page-support__left">
-            <div class="wplp-page-wrapper wplp-d-block wplp-p-0">
-                <h1 class="wplp-support-card-title">Usage</h1>
-                <div class="wplp-support-card-content">
-                    <p class="wplp-mb-20">Using WooCommerce Product Carousel, Slider & Grid is very easy. Just follow the following steps and you are ready to go.</p>
-                    <ul class="wplp-usage-step">
-                        <li><strong>Step-1: </strong>Create new product carousel by clicking on the 'Add New' from the "Woocommerce Product Ultimate" menu.</li>
-                        <li><strong>Step-2: </strong>Customize different options of the carousel and click on the 'Publish' button.</li>
-                        <li><strong>Step-3: </strong>Copy the shortcode from the bottom of the page. For example: [wcpcsu id="123"]</li>
-                        <li><strong>Step-1: </strong>Paste the shortcode on any page or post where you would like to display the product carousel.</li>
-                    </ul>
-                    <div class="wplp-instruction-video wplp-mt-30">
-                        <h2 class="text-center wplp-mb-20">Watch the Video</h2>
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/jnYY3uZLH6c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
             <div class="legal-page-help wplp-support-card wplp-container-sm wplp-mt-30">
                 <div class="wplp-page-wrapper wplp-d-block wplp-p-0">
                     <h1 class="wplp-support-card-title">Need Help?</h1>
                     <div class="wplp-support-card-content">
                         <p class="wplp-mb-20">Need help? Get special care from our dedicated support team.</p>
-                        <a href="" class="wplp-btn-primary">Get Support</a>
+                        <a href="https://wpwax.com/contact/" class="wplp-btn-primary">Get Support</a>
                     </div>
                 </div>
             </div> <!--ends .tab-pane   #Support-->
